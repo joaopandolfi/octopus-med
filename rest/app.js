03a3c859e129f0b25b15e02d0334b9184db15c08
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2017-11-09 13:27:37 -0200

    Https and http server

--- rest/app.js
+++ rest/app.js
@@ -6,6 +6,8 @@
 */
 
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 const config = require("./constants/config.js");
@@ -15,13 +17,27 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+//=== Configuring server
 app.get('/', function (req, res) { res.send('OK'); });
 
 app = require('./routes/defines/specialties')(app);
 
-var server = app.listen(config.port, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+
+//=== Initializing Servers
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(config.credentials, app);
+
+httpServer.listen(config.http_port, function () {
+	var host = httpServer.address().address;
+	var port = httpServer.address().port;
 
 	console.log("Octopus Med listening at http://%s:%s", host, port);
+});
+
+httpsServer.listen(config.https_port, function () {
+	var host = httpsServer.address().address;
+	var port = httpsServer.address().port;
+
+	console.log("Octopus Med listening at https://%s:%s", host, port);
 });
--- rest/constants/config.js
+++ rest/constants/config.js
@@ -1,3 +1,12 @@
+var fs = require('fs');
+
 module.exports = {
-	port: 3000
+	http_port: 3000,
+	https_port: 8443,
+	/*https_options: {
+	  ca: [fs.readFileSync(PATH_TO_BUNDLE_CERT_1), fs.readFileSync(PATH_TO_BUNDLE_CERT_2)],
+      cert: fs.readFileSync(PATH_TO_CERT),
+      key: fs.readFileSync(PATH_TO_KEY)
+	}*/
+	credentials : {key: fs.readFileSync('./constants/sslcert/server.key', 'utf8'), cert: fs.readFileSync('./constants/sslcert/server.crt', 'utf8')}
 }
