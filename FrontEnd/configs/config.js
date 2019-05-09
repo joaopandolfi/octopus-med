f0122bf27e8c91528124bb760cf1305f3d552a43
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2019-05-09 16:11:50 -0300

    Ips

--- FrontEnd/configs/config.js
+++ FrontEnd/configs/config.js
@@ -4,8 +4,8 @@
  * */
 
 module.exports = {
-    _server : "http://localhost:9091", //"http://31.220.54.251:9092" 
-    _server_front : "http://localhost:3000",
-    _server_back : "http://localhost:9091",//"http://31.220.54.251:9092",
+    _server : "http://31.220.54.251:9092", //"http://31.220.54.251:9092" 
+    _server_front : "http://31.220.54.251:3000",
+    _server_back : "http://31.220.54.251:9092",//"http://31.220.54.251:9092",
     _port: 3024,
 }
