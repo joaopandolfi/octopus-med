768d56ba2699b7acc9c54f9cffc86785048dd21b
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2019-05-09 18:07:59 -0300

    View

--- FrontEnd/public/js/conduct.js
+++ FrontEnd/public/js/conduct.js
@@ -12,7 +12,6 @@ function initConducts(callback){
                             <p>${m.name}</p>
                             <ul class="card-just">
                             <li><i class="fas fa-caret-right"></i> ${m.name} - ${m.nameUserDone} às ${m.doneDateTime}</li>
-                                   <--! <li><i class="fas fa-caret-right"></i> Solicitar avaliação da cirurgia vascular - Priscilla Aquino às 16:30h</li> -->
                             </ul>
                         </li>`
 
@@ -46,11 +45,10 @@ function initConducts(callback){
                             <p>${m.name}</p>
                             <ul class="card-just">
                             <li><i class="fas fa-caret-right"></i> ${m.name} - ${m.nameUserDone} às ${m.doneDateTime}</li>
-                                   <--! <li><i class="fas fa-caret-right"></i> Solicitar avaliação da cirurgia vascular - Medico1 às 16:30h</li> -->
                             </ul>
                             <div class="rw card-check">
-                                <label class="checks"><input type="checkbox" checked="checked" onchange="toggle(this,${m.Id})"><span class="checkmark"></span></label>
-                                <a href="#" onclick="edit(${m.Id})"><i class="far fa-edit"></i></a>
+                                <label class="checks"><input type="checkbox" style="width:28px" checked="checked" onchange="toggle(this,${m.Id})"><span class="checkmark"></span></label>
+                                <a href="#" onclick="edit('${m.Id}')"><i class="far fa-edit"></i></a>
                                 <a href="#"><i class="fas fa-comments"></i></a>
                             </div>
                         </li>`
