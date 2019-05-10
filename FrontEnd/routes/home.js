1298ce690c9765cb8cec6cab7ea8b4321459df78
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2019-05-09 21:18:11 -0300

    login page

--- FrontEnd/routes/home.js
+++ FrontEnd/routes/home.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 var navigationController = require("../controllers/navigationController")
 var authController = require("../controllers/authController")
 
-router.get('/', navigationController.Home)
+router.get('/',authController.Auth, navigationController.Home)
 
 router.get('/local', authController.Auth, navigationController.WorkLocal)
 router.get('/config/:hospital', authController.Auth, navigationController.Config)
